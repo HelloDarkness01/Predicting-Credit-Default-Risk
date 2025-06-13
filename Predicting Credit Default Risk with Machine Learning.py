748239dd30f7f617e7f0c331c8f3a893f19a4e1e
Satkrit Bhattarai <satbhattarai01@gmail.com>
2025-06-13 13:49:14 -0400

    Initial commit

---
+++ Predicting Credit Default Risk with Machine Learning.py
@@ -0,0 +1,12 @@
+# Phase 1: Imports and Setup
+# Import necessary libraries
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from scipy.stats import norm
+from scipy.stats import chi2
+import scipy.stats as stats
